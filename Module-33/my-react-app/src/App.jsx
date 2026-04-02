e014e1ad6480b75636f5f7c803b7f4dbcd85f96d
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-03 01:44:16 +0600

    commit

--- Module-33/my-react-app/src/App.jsx
+++ Module-33/my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -21,6 +22,7 @@ function App() {
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p> */}
         </div>
+        <countries></countries>
         {/* <button
           className="counter"
           onClick={() => setCount((count) => count + 1)}
--- Module-33/my-react-app/src/components/countrys/counttris.jsx
+++ Module-33/my-react-app/src/components/countrys/counttris.jsx
@@ -1,7 +1,11 @@
-function Countries() {
+import React from 'react';
+
+const counttris = () => {
   return (
     <div>
-      
+      <h1>Hello</h1>
     </div>
-  )
-}
+  );
+};
+
+export default counttris;
