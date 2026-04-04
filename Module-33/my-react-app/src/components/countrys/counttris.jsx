1ef59092e44a987cf239dc8368dd75a4792cba23
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-05 00:34:59 +0600

    commit

--- Module-33/my-react-app/src/components/countrys/counttris.jsx
+++ Module-33/my-react-app/src/components/countrys/counttris.jsx
@@ -1,14 +1,16 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
 
 const Counttris = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise)
   const countries = countriesData.countries;
-  console.log(countries);
 
   return (
     <div>
       <h1>Our countries: {countries.length}</h1>
-      <h1>Our countries: {countries.name}</h1>
+      {
+        countries.map(country => <Country country></Country>)
+      }
     </div>
   );
 };
