62a02beb8b386907067bab08a85c090bd5cd1991
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-06 01:35:45 +0600

    commit

---
+++ Module-34/tailwind-react-pricing-option/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        
+        <div>
+          <h1 className='text-5xl'>Get started</h1>
+          
+        </div>
+        
+      </section>
+
+     
+
+      
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
