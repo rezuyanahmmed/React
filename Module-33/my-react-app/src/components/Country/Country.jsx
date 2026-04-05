0bcec0ed89dc9792851dfffc2ac643ef58a1795c
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-05 23:51:07 +0600

    commit

--- Module-33/my-react-app/src/components/Country/Country.jsx
+++ Module-33/my-react-app/src/components/Country/Country.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Country.css'
 const Country = ({ country }) => {
   return (
-   
+    <div className='country'>
+      <img src={country.flags.flags.png} alt="" />
+      <h3>name: {country.name.common}</h3>
+      <p>Popolation: {country.population.population} </p>
+    </div>
   );
 };
 
