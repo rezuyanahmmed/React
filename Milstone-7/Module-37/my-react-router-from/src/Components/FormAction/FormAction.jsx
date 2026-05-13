010266b586e868e01a8593bdea07b8dac624ac03
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-14 00:46:09 +0600

    commit

--- Milstone-7/Module-37/my-react-router-from/src/Components/FormAction/FormAction.jsx
+++ Milstone-7/Module-37/my-react-router-from/src/Components/FormAction/FormAction.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const FormAction = () => {
+
+  const handleFormAction = (formData) =>{
+    console.log(formData);
+    
+  }
+
   return (
     <div>
       <form action="">
-        <input type="text" placeholder='Name'/>
+        <input type="text" placeholder='Name' />
         <input type="email" placeholder='Email' />
       </form>
     </div>
