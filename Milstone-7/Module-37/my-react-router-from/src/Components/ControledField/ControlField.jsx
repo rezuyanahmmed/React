b29e97911c891afd478683e5853a919b36f18374
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-14 00:58:35 +0600

    commit

--- Milstone-7/Module-37/my-react-router-from/src/Components/ControledField/ControlField.jsx
+++ Milstone-7/Module-37/my-react-router-from/src/Components/ControledField/ControlField.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const ControlField = () => {
   return (
     <div>
-      
+      <form>
+        <input type="email" name="email" id="" placeholder='Email' required/>
+        <br />
+        <input type="password" name="password" id="" placeholder='password' required/>
+        <br />
+        <input type="submit" value="" />
+      </form>
     </div>
   );
 };
