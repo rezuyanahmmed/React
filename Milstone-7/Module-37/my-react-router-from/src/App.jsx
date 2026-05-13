a7f5be28d3d2ff531d7a3a7e5ba6c0992551ef51
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-14 00:54:08 +0600

    commit

--- Milstone-7/Module-37/my-react-router-from/src/App.jsx
+++ Milstone-7/Module-37/my-react-router-from/src/App.jsx
@@ -1,20 +1,22 @@
 
 import './App.css'
+import ControlField from './Components/ControledField/ControlField'
 import FormAction from './Components/FormAction/FormAction'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
- 
+
 
   return (
     <>
-     
-      
-     <h2>Explore React form</h2>
-     {/* <SimpleForm></SimpleForm> */}
-     <FormAction></FormAction>
 
-      
+
+      <h2>Explore React form</h2>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <FormAction></FormAction> */}
+      <ControlField></ControlField>
+
+
     </>
   )
 }
