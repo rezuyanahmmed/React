66a1f2a275872e7f461e9837bc65841a5bddee62
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-14 01:48:54 +0600

    commit

--- Milstone-7/Module-37/my-react-router-from/src/App.jsx
+++ Milstone-7/Module-37/my-react-router-from/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ControlField from './Components/ControledField/ControlField'
 import FormAction from './Components/FormAction/FormAction'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
+import UnControlledField from './Components/unControlledField/UnControlledField'
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
       <h2>Explore React form</h2>
       {/* <SimpleForm></SimpleForm> */}
       {/* <FormAction></FormAction> */}
-      <ControlField></ControlField>
+      {/* <ControlField></ControlField> */}
+      <UnControlledField></UnControlledField>
 
 
     </>
