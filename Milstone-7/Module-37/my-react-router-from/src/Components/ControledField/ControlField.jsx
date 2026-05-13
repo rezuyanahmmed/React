9ee47b2a3b4f34115c03e5a2faf000c0f21b3584
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-14 01:36:54 +0600

    commit

--- Milstone-7/Module-37/my-react-router-from/src/Components/ControledField/ControlField.jsx
+++ Milstone-7/Module-37/my-react-router-from/src/Components/ControledField/ControlField.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ControlField = () => {
+
+  const [password, setpassword] = useState('')
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefult();
+
+    if (password.length < 6) {
+      setError('password must be 6 charecters')
+    } else {
+      setError('')
+    }
+
+  }
+
+  const handlePasswordOnchange = e => {
+    console.log(e.target.value);
+    setpassword(e.target.value);
+
+    // if (password.length < 6) {
+    //   setError('password must be 6 charecters')
+    // } else {
+    //   setError('')
+    // }
+
+  }
+
+
   return (
     <div>
-      <form>
-        <input type="email" name="email" id="" placeholder='Email' required/>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" id="" placeholder='Email' required />
         <br />
-        <input type="password" name="password" id="" placeholder='password' required/>
+        <input type="password" name="password" id="" placeholder='password'
+          onChange={handlePasswordOnchange} defaultValue={password} required />
         <br />
-        <input type="submit" value="" />
+        <input type="submit" value="Submit" />
       </form>
+      <p style={{ color: 'red' }}>
+        <small>{error}</small>
+      </p>
     </div>
   );
 };
