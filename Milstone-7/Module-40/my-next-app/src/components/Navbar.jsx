a995af81518e1815607cc2076a760ef652320fea
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-06-28 23:36:04 +0600

    commit

--- Milstone-7/Module-40/my-next-app/src/components/Navbar.jsx
+++ Milstone-7/Module-40/my-next-app/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn"></a>
         </div>
       </div>
     </div>
