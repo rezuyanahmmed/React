2f5f74f6e99dd765ec923b0ea2c38de96596552b
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-07-13 20:49:19 +0600

    commit

---
+++ Milstone-7/Module-40/my-next-app/src/app/dashboard/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h2>update your profile</h2>
+    </div>
+  );
+};
+
+export default ProfilePage;
--- Milstone-7/Module-40/my-next-app/src/components/Navbar.jsx
+++ Milstone-7/Module-40/my-next-app/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
     <li><Link href='/contact'>Contact</Link></li>
     <li><Link href='/blogs'>Blogs</Link></li>
     <li><Link href='/about/developers'>Developers</Link></li>
+    <li><Link href='/dashboard'>Dashboard</Link></li>
 
   </>
 
